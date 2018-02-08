478d8d05fd2dbeb4db2f219919bc90a8f29fb4e0
Povilas Skruibis <povilass@wix.com>
2018-02-08 15:49:16 +0200

    Move game to library

--- rust/src/trivia.rs
+++ rust/src/trivia.rs
@@ -1,8 +1,10 @@
 #[macro_use]
 extern crate serde_derive;
 
+pub mod game;
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Execution {
   pub seed: usize,
-  pub output: String
+  pub output: String,
 }
