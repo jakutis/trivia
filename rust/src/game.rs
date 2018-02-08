df89d96f4d8e36455936b998fe421d76d7976c6e
Vytautas Jakutis <jakutis@wix.com>
2018-02-08 16:21:15 +0200

    replace println with self.log

--- rust/src/game.rs
+++ rust/src/game.rs
@@ -41,6 +41,10 @@ impl Default for Game {
 }
 
 impl Game {
+  fn log(&self, line: String) {
+    println!("{}", line);
+  }
+
   fn how_many_players(&self) -> usize {
     self.players.len()
   }
@@ -68,14 +72,14 @@ impl Game {
     self.places[self.how_many_players()] = 0;
     self.purses[self.how_many_players()] = 0;
     self.in_penaltybox[self.how_many_players()] = false;
-    println!("{} was added", l_player);
-    println!("They are player number {}", self.players.len());
+    self.log(format!("{} was added", l_player));
+    self.log(format!("They are player number {}", self.players.len()));
     true
   }
 
   pub fn wrong_answer(&mut self) -> bool {
-    println!("Question was incorrectly answered");
-    println!("{} was sent to the penalty box", self.players[self.current_player as usize]);
+    self.log(format!("Question was incorrectly answered"));
+    self.log(format!("{} was sent to the penalty box", self.players[self.current_player as usize]));
     self.in_penaltybox[self.current_player as usize] = true;
     self.current_player += 1;
     if self.current_player == self.players.len() as i32 {
@@ -90,22 +94,22 @@ impl Game {
     match self.current_category() {
       "Pop" => {
         let top = self.pop_questions.pop();
-        println!("{:?}", top.unwrap());
+        self.log(format!("{:?}", top.unwrap()));
       }
       "Science" => {
         let top = self.science_questions.pop();
-        println!("{:?}", top.unwrap());
+        self.log(format!("{:?}", top.unwrap()));
       }
       "Sports" => {
         let top = self.sports_questions.pop();
-        println!("{:?}", top.unwrap());
+        self.log(format!("{:?}", top.unwrap()));
       }
       "Rock" => {
         let top = self.rock_questions.pop();
-        println!("{:?}", top.unwrap());
+        self.log(format!("{:?}", top.unwrap()));
       }
       _ => {
-        println!("Unexpected case");
+        self.log(format!("Unexpected case"));
       }
     }
   }
@@ -113,21 +117,21 @@ impl Game {
 
 impl Game {
   pub fn roll(&mut self, roll: i32) {
-    println!("{} is current player", self.players[self.current_player as usize]);
-    println!("They have rolled a {}", roll);
+    self.log(format!("{} is current player", self.players[self.current_player as usize]));
+    self.log(format!("They have rolled a {}", roll));
     if self.in_penaltybox[self.current_player as usize] {
       if roll % 2 != 0 {
         self.is_getting_out_of_penaltybox = true;
-        println!("{} is getting out of the penalty box", self.players[self.current_player as usize]);
+        self.log(format!("{} is getting out of the penalty box", self.players[self.current_player as usize]));
         self.places[self.current_player as usize] += roll;
         if self.places[self.current_player as usize] > 11 {
           self.places[self.current_player as usize] -= 12;
         }
-        println!("{0} 's new location is {1}", self.players[self.current_player as usize], self.places[self.current_player as usize]);
-        println!("The category is {}", self.current_category());
+        self.log(format!("{0} 's new location is {1}", self.players[self.current_player as usize], self.places[self.current_player as usize]));
+        self.log(format!("The category is {}", self.current_category()));
         self.ask_question();
       } else {
-        println!("{} is not getting out of the penalty box", self.players[self.current_player as usize]);
+        self.log(format!("{} is not getting out of the penalty box", self.players[self.current_player as usize]));
         self.is_getting_out_of_penaltybox = false;
       }
     } else {
@@ -135,8 +139,8 @@ impl Game {
       if self.places[self.current_player as usize] > 11 {
         self.places[self.current_player as usize] -= 12;
       }
-      println!("{0} 's new location is {1}", self.players[self.current_player as usize], self.places[self.current_player as usize]);
-      println!("The category is {}", self.current_category());
+      self.log(format!("{0} 's new location is {1}", self.players[self.current_player as usize], self.places[self.current_player as usize]));
+      self.log(format!("The category is {}", self.current_category()));
       self.ask_question();
     }
   }
@@ -146,9 +150,9 @@ impl Game {
   pub fn was_correctly_answered(&mut self) -> bool {
     if self.in_penaltybox[self.current_player as usize] {
       if self.is_getting_out_of_penaltybox {
-        println!("Answer was correct!!!!");
+        self.log(format!("Answer was correct!!!!"));
         self.purses[self.current_player as usize] += 1;
-        println!("{0} now has {1} Gold Coins.", self.players[self.current_player as usize], self.purses[self.current_player as usize]);
+        self.log(format!("{0} now has {1} Gold Coins.", self.players[self.current_player as usize], self.purses[self.current_player as usize]));
         let winner: bool = self.did_player_win();
         self.current_player += 1;
         if self.current_player == self.players.len() as i32 {
@@ -163,9 +167,9 @@ impl Game {
         true
       }
     } else {
-      println!("Answer was correct!!!!");
+      self.log(format!("Answer was correct!!!!"));
       self.purses[self.current_player as usize] += 1;
-      println!("{0} now has {1} Gold Coins.", self.players[self.current_player as usize], self.purses[self.current_player as usize]);
+      self.log(format!("{0} now has {1} Gold Coins.", self.players[self.current_player as usize], self.purses[self.current_player as usize]));
       let winner: bool = self.did_player_win();
       self.current_player += 1;
       if self.current_player == self.players.len() as i32 {
